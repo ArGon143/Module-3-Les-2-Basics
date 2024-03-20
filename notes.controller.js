acbeebeafc53d6e179caeac8e8f17d5b29bb987f
argon <argon143@mail.ru>
2024-03-20 20:19:10 +0300

    app

---
+++ notes.controller.js
@@ -0,0 +1,56 @@
+const fs = require("fs/promises");
+const path = require("path");
+const chalk = require("chalk");
+
+const notesPath = path.join(__dirname, "db.json");
+
+async function addNote(title) {
+  const notes = await getNotes();
+
+  const note = {
+    title,
+    id: Date.now().toString(),
+  };
+
+  notes.push(note);
+
+  await fs.writeFile(notesPath, JSON.stringify(notes));
+  console.log(chalk.bgGreen("Note was added!"));
+}
+
+async function removeNote(id) {
+  const notes = await getNotes();
+  const notesCopy = [...notes];
+
+  for (let i = 0; i < notesCopy.length; i++) {
+    if (notesCopy[i].id === String(id)) {
+      notesCopy.splice(i, 1);
+
+      break;
+    }
+  }
+
+  await fs.writeFile(notesPath, JSON.stringify(notesCopy));
+  console.log(chalk.bgRed("Note was removed!"));
+}
+
+async function getNotes() {
+  const notes = await fs.readFile(notesPath, { encoding: "utf-8" });
+
+  return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : [];
+}
+
+async function printNotes() {
+  const notes = await getNotes();
+  console.log(chalk.bgBlue("Here is the list notes:"));
+
+  notes.forEach((note) => {
+    console.log(chalk.blue(note.id, note.title));
+  });
+}
+
+module.exports = {
+  addNote,
+  printNotes,
+  removeNote,
+};
